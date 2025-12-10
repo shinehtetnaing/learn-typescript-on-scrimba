4c5764270cbb2259ac38d50adcedd30432a057d5
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-12-10 12:55:46 +0700

    intro to pizza app

--- index.js
+++ index.js
@@ -6,4 +6,57 @@ const menu = [
 ];
 
 const cashInRegister = 100;
+const nextOrderId = 1;
 const orderQueue = [];
+
+/**
+ * Challenge: Add a utility function "addNewPizza" that takes a pizza object
+ * and adds it to the menu.
+ */
+const addNewPizza = (pizza) => {
+  menu.push(pizza);
+};
+
+/**
+ * Write another utility function, placeOrder, that takes a pizza name parameter and:
+ * 1. finds that pizza object in the menu,
+ * 2. adds the income to the cashInRegister,
+ * 3. pushes a new "order object" to the orderQueue
+ *   (e.g. { pizza: selectedPizzaObjectFromStep1, status: "ordered" })
+ * 4. returns the new order object (just in case we need it later)
+ */
+
+const placeOrder = (pizzaName) => {
+  const selectedPizza = menu.find((pizza) => pizza.name === pizzaName);
+  cashInRegister += selectedPizza.price;
+  const newOrder = {
+    id: nextOrderId++,
+    pizza: selectedPizza,
+    status: "ordered",
+  };
+  orderQueue.push(newOrder);
+  return newOrder;
+};
+
+/**
+ * Challenge: write another utility function, completeOrder, that takes an orderId as a parameter
+ * finds the correct order in the orderQueue, and marks its status as "completed". For good measure,
+ * return the found order from the function.
+ *
+ * Note: you'll need to ensure that we're adding IDs to our orders when we create new orders. You can use a global `nextOrderId` variable and increment it every time a new order is created to simulate real IDs being managed for us by a database.
+ */
+const completeOrder = (orderId) => {
+  const order = orderQueue.find((order) => order.id === orderId);
+  order.status = "completed";
+  return order;
+};
+
+addNewPizza({ name: "BBQ Chicken", price: 11 });
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 });
+
+placeOrder("Chicken Bacon Ranch");
+completeOrder("1");
+
+console.log("Menu:", menu);
+console.log("Cash in register:", cashInRegister);
+console.log("Order queue:", orderQueue);
